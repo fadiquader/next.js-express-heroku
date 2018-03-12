4ab19eee67656c3ebeeff2cc6571acb768794e78
fadiquader <fadi.quader@gmail.com>
2018-03-12 14:23:46 +0200

    a

--- pages/index.js
+++ pages/index.js
@@ -1,87 +1,117 @@
+import React from 'react';
 import Link from 'next/link'
 import { Button } from 'antd';
 import Head from '../components/head'
 import Nav from '../components/nav'
 import '../styles/ant.less';
 
-export default () => (
-  <div>
-    <Head title="Home" />
-    <Nav />
 
-    <div className="hero">
-      <Button onClick={() => console.log('antd works fine')}>Antd Button</Button>
-      <h1 className="title">Welcome to Next!</h1>
-      <p className="description">To get started, edit <code>pages/index.js</code> and save to reload.</p>
+class Home extends React.Component {
 
-      <div className="row">
-        <Link href="https://github.com/zeit/next.js#getting-started">
-          <a className="card">
-            <h3>Getting Started &rarr;</h3>
-            <p>Learn more about Next on Github and in their examples</p>
-          </a>
-        </Link>
-        <Link href="https://open.segment.com/create-next-app">
-          <a className="card">
-            <h3>Examples &rarr;</h3>
-            <p>
-              Find other example boilerplates on the <code>create-next-app</code> site
-            </p>
-          </a>
-        </Link>
-        <Link href="https://github.com/segmentio/create-next-app">
-          <a className="card">
-            <h3>Create Next App &rarr;</h3>
-            <p>Was this tool helpful? Let us know how we can improve it</p>
-          </a>
-        </Link>
-      </div>
-    </div>
+    static async getInitialProps({req}) {
+        // const data = await require('../data/table.json')
+        const res = await fetch(`https://jsonplaceholder.typicode.com/comments`);
+        const data = await res.json();
+        return {
+            data
+        }
+    }
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title, .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9B9B9B;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-)
+    constructor(props) {
+        super(props);
+        this.state = {
+            filters: {
+                completed: false,
+            },
+            data: props.data
+        };
+
+        this.onSortChange = this.onSortChange.bind(this)
+        this.onfilterChange = this.onfilterChange.bind(this)
+    }
+
+    onSortChange(e){
+        console.log(e.target.checked, e.target.name)
+        this.setState({
+            sortedInfo: {
+                order: !e.target.checked ? 'descend' : 'ascend',
+                columnKey: e.target.name,
+            },
+        });
+    }
+
+    onfilterChange(e) {
+        const filters = {};
+        filters[e.target.name] = e.target.checked;
+        const data = [];
+        this.props.data.map((record) => {
+            if(!filters.completed) return data.push(record);
+            if(filters.completed === record.completed)  return data.push(record);
+        });
+        this.setState(prev => ({
+            filters: { ...prev.filters, ...filters },
+            data: data
+        }))
+    }
+
+    render() {
+        const columns = [
+            {
+                title: 'ID',
+                dataIndex: 'id',
+                key: 'key',
+            },
+            {
+                title: 'Post ID',
+                dataIndex: 'postId',
+                key: 'postId',
+            },
+            {
+                title: 'Email',
+                dataIndex: 'email',
+                key: 'email',
+            },
+            {
+                title: 'Body',
+                dataIndex: 'body',
+                key: 'body',
+            },
+            // {
+            // title: 'Completed',
+            // dataIndex: 'completed',
+            // key: 'completed',
+            // render: (text, record) => `${text}`
+            // }
+        ];
+        return (
+            <div className="container">
+              <Row type="flex" gutter={32}>
+                <Col span={0}>
+                  <div>
+                      {/*<Checkbox name="completed" onChange={this.onfilterChange}>Filter by completed</Checkbox>*/}
+                  </div>
+                </Col>
+                <Col span={24}>
+                  <Table
+                      pagination={false}
+                      columns={columns}
+                      dataSource={this.state.data}
+                  />
+                </Col>
+              </Row>
+              <style jsx>
+                  {
+                      `
+          .container {
+          width: 75%;
+          margin: 20px auto
+        }
+          `
+                  }
+              </style>
+            </div>
+        );
+    }
+}
+
+export default Home
